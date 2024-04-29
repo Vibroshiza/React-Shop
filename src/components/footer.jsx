36101380b9d5fd4e9dc106ba331abf210c65c8a3
Марго <121296275+Vibroshiza@users.noreply.github.com>
2024-04-29 13:06:27 +0300

    Final-Commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    return <footer className="page-footer margin-bottom red-back">
+        <div className="container">
+            <div className="row">
+                <div className="col l6 s12">
+                    <h5 className="white-text">Footer Moment</h5>
+                    <p className="grey-text text-lighten-4">I don't know what to wright here, this footer just looks cool.</p>
+                </div>
+                <div className="col l4 offset-l2 s12"></div>
+            </div>
+        </div>
+        <div className="footer-copyright">
+            <div className="container">
+                © 2014 Copyright bla-bla-bla
+                <a className="grey-text text-lighten-4 right" href="#!">Made by: ViBrO</a>
+            </div>
+        </div>
+    </footer>
+}
+
+export { Footer };
